2276e1f23f6d59c76aa14e5e771b292f5ce083bb
chopperkuang <chopperkuang@gmail.com>
2013-02-21 21:37:45 +0800

    fix component and format route

--- js/CustomerService.js
+++ js/CustomerService.js
@@ -8,7 +8,6 @@ angularDemo.service('CustomerService', function CustomerService($http, $resource
 		var api_myPrivate = "http://10.8.11.34:8090/api/app/myPrivate/"+ empNo +"?callback=JSON_CALLBACK";
 	    return $http.jsonp(api_myPrivate).then(
 	    	function(response) { //success
-	    		console.log("xxx");	    	
 	        	return response.data.inquiryList;	         
 	    	}, 
 	    	function(status) { //error
