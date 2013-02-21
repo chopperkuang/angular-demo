7cc3799be03b29d0548149342c03657b9ddd6e59
chopperkuang <chopperkuang@gmail.com>
2013-02-21 14:03:18 +0800

    add component

--- js/controller.js
+++ js/controller.js
@@ -2,14 +2,14 @@ function CustomerListCtrl($scope, $http) {
     
     $scope.show = "hidden";
 
-    var api_myPrivate = "http://10.8.11.41:8090/api/app/myPrivate/83639?callback=JSON_CALLBACK";
+    var api_myPrivate = "http://10.8.11.34:8090/api/app/myPrivate/83639?callback=JSON_CALLBACK";
     $http.jsonp(api_myPrivate).success(function(data) {
         $scope.customers = data.inquiryList;
         
     });
 
     $scope.call = function(num) {
-        $http.jsonp("http://10.8.11.41:8090/api/app/" + num + "/phone?callback=JSON_CALLBACK").success(function(data) {
+        $http.jsonp("http://10.8.11.34:8090/api/app/" + num + "/phone?callback=JSON_CALLBACK").success(function(data) {
             console.log(data.phones[0].phoneNumber);
             $scope.phoneNumber = data.phones[0].phoneNumber;   
         });
